badae64e0626d23d8d549faffc745c4b48c47eee
neon <slava.v.ivannikov@gmail.com>
2023-08-17 21:23:10 +0200

    added img2img

--- src/animatediff/train_utils/dataset.py
+++ src/animatediff/train_utils/dataset.py
@@ -1,24 +1,27 @@
+import urllib.request
+
 import decord
-decord.bridge.set_bridge('torch')
+import pandas as pd
 
 from torch.utils.data import Dataset
 from einops import rearrange
 
+decord.bridge.set_bridge('torch')
+
 
 class TuneAVideoDataset(Dataset):
     def __init__(
-            self,
-            video_path: str,
-            prompt: str,
-            width: int = 512,
-            height: int = 512,
-            n_sample_frames: int = 8,
-            sample_start_idx: int = 0,
-            sample_frame_rate: int = 1,
+        self,
+        csv_path: str,
+        prompt: str,
+        width: int = 512,
+        height: int = 512,
+        n_sample_frames: int = 8,
+        sample_start_idx: int = 0,
+        sample_frame_rate: int = 1,
     ):
-        self.video_path = video_path
-        self.prompt = prompt
-        self.prompt_ids = None
+        df = pd.read_csv(csv_path)
+        self.items = df.to_dict('records')
 
         self.width = width
         self.height = height
@@ -27,11 +30,24 @@ class TuneAVideoDataset(Dataset):
         self.sample_frame_rate = sample_frame_rate
 
     def __len__(self):
-        return 1
+        return len(self.items)
+
+    def remove_text(self, filename):
+        pass
+
+    def fetch_video(self, index, filename="tmp.mp4"):
+        url = self.items[index]["contentUrl"]
+        caption = self.items[index]["name"]
+
+        urllib.request.urlretrieve(url, filename)
+
+        self.remove_text(filename)
+        return filename, caption
 
     def __getitem__(self, index):
         # load and sample video frames
-        vr = decord.VideoReader(self.video_path, width=self.width, height=self.height)
+        video_path, caption = self.fetch_video(index)
+        vr = decord.VideoReader(video_path, width=self.width, height=self.height)
         sample_index = list(range(self.sample_start_idx, len(vr), self.sample_frame_rate))[:self.n_sample_frames]
         video = vr.get_batch(sample_index)
         video = rearrange(video, "f h w c -> f c h w")
--- train.py
+++ train.py
@@ -1,5 +1,4 @@
 import argparse
-import datetime
 import logging
 import inspect
 import math
